d4d5f860ba958e637b16e7ccb5ce45a56c5b10fa
abdellahsinaoui1-lang <abdellahsinaoui1@gmail.com>
2026-07-18 18:02:04 +0100

    Refactor error handling and add ALLOWED_ROLE_ID

--- config.py
+++ config.py
@@ -3,12 +3,10 @@ import os
 TOKEN = os.getenv("TOKEN")
 GROQ_KEY = os.getenv("GROQ_KEY")
 
-MODEL = "meta-llama/llama-4-scout-17b-16e-instruct"
+ALLOWED_ROLE_ID = 1528082462897475674
 
 if TOKEN is None:
-    raise Exception("TOKEN variable not found.")
+    raise ValueError("TOKEN not found.")
 
 if GROQ_KEY is None:
-    raise Exception("GROQ_KEY variable not found.")
-    # Role allowed to use F7 Bot
-ALLOWED_ROLE_ID = 1528082462897475674
+    raise ValueError("GROQ_KEY not found.")
