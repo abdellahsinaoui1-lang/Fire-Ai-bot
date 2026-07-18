80958d921e5080b844ec663deb8ff13dae67b2ef
abdellahsinaoui1-lang <abdellahsinaoui1@gmail.com>
2026-07-18 17:34:30 +0100

    Add environment variable checks in config.py
    Added checks for TOKEN and GROQ_KEY environment variables.

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
 
-MODEL = "meta-llama/llama-4-scout-17b-16e-instruct"
-
 TOKEN = os.getenv("TOKEN")
 GROQ_KEY = os.getenv("GROQ_KEY")
+
+MODEL = "meta-llama/llama-4-scout-17b-16e-instruct"
+
+if TOKEN is None:
+    raise Exception("TOKEN variable not found.")
+
+if GROQ_KEY is None:
+    raise Exception("GROQ_KEY variable not found.")
