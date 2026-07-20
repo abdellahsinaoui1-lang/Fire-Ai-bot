f71e757e7f40e93d10e77994b627ce61bf2ca3e2
abdellahsinaoui1-lang <abdellahsinaoui1@gmail.com>
2026-07-20 23:06:58 +0100

    Add async create_category function for guild

--- executor/create_category.py
+++ executor/create_category.py
@@ -1,1 +1,9 @@
+async def create_category(guild, message, data):
 
+    name = data["Name"]
+
+    await guild.create_category(name=name)
+
+    await message.reply(
+        f"✅ تم إنشاء الكاتيجوري **{name}**."
+    )
