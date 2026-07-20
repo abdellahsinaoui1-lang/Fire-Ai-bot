f8e0854780c65ce9e2a1320e3e54a614e2ade998
abdellahsinaoui1-lang <abdellahsinaoui1@gmail.com>
2026-07-20 23:05:04 +0100

    Add execute_command function for bot commands

--- executor/__init__.py
+++ executor/__init__.py
@@ -1,1 +1,22 @@
+from .create_channel import create_channel
+from .create_category import create_category
+from .create_role import create_role
 
+
+async def execute_command(bot, message, command):
+
+    for key, data in command.items():
+
+        if key.startswith("CreateChannel"):
+            await create_channel(message.guild, message, data)
+
+        elif key.startswith("CreateCategory"):
+            await create_category(message.guild, message, data)
+
+        elif key.startswith("CreateRole"):
+            await create_role(message.guild, message, data)
+
+        else:
+            await message.reply(
+                f"❌ الأمر **{key}** غير مدعوم حالياً."
+            )
